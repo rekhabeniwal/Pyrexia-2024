fabcad918569ea724d8f5fd1b1603e18f1f1e098
Rekha <136164355+rekha-beniwal@users.noreply.github.com>
2024-09-18 02:47:44 +0530

    Update BasicRegistration.js

--- frontend/src/components/BasicRegistration.js
+++ frontend/src/components/BasicRegistration.js
@@ -30,11 +30,11 @@ const BasicRegistration = () => {
 
           {/* Register Button */}
           <div className="flex justify-center items-center mt-10 p-4">
-            <button
-              
+            <button>
+            <a 
               className="bg-[#ebe6d0] hover:bg-gradient-to-t from-blue-500 to-white text-black px-6 py-2.5 rounded-lg font-bold text-sm border-black hover:bg-[#d9d2b8] transition duration-300"
-            >
-              <a href="https://forms.gle/PRjwsH44sZiHBaUr5">Register Now</a>
+
+              href="https://forms.gle/PRjwsH44sZiHBaUr5">Register Now</a>
             </button>
           </div>
 
