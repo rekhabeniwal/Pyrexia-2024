79482ddf280fbae9db5756b32447d7f228410700
Rekha <136164355+rekha-beniwal@users.noreply.github.com>
2024-09-18 14:07:04 +0530

    Update login.js

--- frontend/src/LoginandSignup/login.js
+++ frontend/src/LoginandSignup/login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import "./login.css";
-
+import {BASE_URL} from "../BaseUrl"
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -13,7 +13,7 @@ const Login = () => {
     const handleLogin = async(e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:6005/login', { email, password });
+            const response = await axios.post(``${BASE_URL}/login`, { email, password });
             alert(response.data.message);
             if (response.data.success) {
                 navigate('/'); // redirect to dashboard on successful login
@@ -26,7 +26,7 @@ const Login = () => {
     };
 
     const loginWithGoogle = () => {
-        window.open("http://localhost:6005/auth/google/callback", "_self");
+        window.open(``${BASE_URL}/auth/google/callback`, "_self");
     };
 
     return (
